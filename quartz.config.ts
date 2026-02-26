6e7b04aa529db81ac4713462e9d5c6cc2a87a88b
ttlojs-john <ttlojs@googlemail.com>
2026-02-26 12:43:51 +0100

    ok

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "doc.chaeyul.uk",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
